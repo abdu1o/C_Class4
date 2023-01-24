8bb7eb67f76875efc604c82ff91a846f141d7c3f
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-01-24 18:53:38 +0200

    Добавьте файлы проекта.

---
+++ С_Сlass4/С_Сlass4.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+class Overcoat
+{
+private:
+    int value;
+    string type;
+
+public:
+
+    Overcoat(int _value, string _type)
+    {
+        value = _value;
+        type = _type;
+    }
+
+    void Print()
+    {
+        cout << type << " " << value << endl;
+    }
+
+    bool operator == (const Overcoat& other)
+    {
+        return this->type == other.type;
+    }
+
+    void operator = (const Overcoat& other)
+    {
+        this->value = other.value;
+        this->type = other.type;
+    }
+
+    bool operator > (const Overcoat& other)
+    {
+        return this->value > other.value;
+    }
+};
+
+
+int main()
+{
+    Overcoat overcoat1(2000, "jacket");
+    Overcoat overcoat2(4000, "coat");
+
+    overcoat1.Print();
+    overcoat2.Print();
+
+    bool result1 = overcoat1 == overcoat2;
+    cout << "Is overcoat1 == overcoat2: " << result1 << endl << endl;
+
+    overcoat1 = overcoat2;
+    overcoat1.Print();
+    overcoat2.Print();
+
+    bool result2 = overcoat1 > overcoat2;
+    cout << "Is value of overcoat1 > overcoat2: " << result2 << endl;
+}
